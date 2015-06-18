931fa355115052c8a872f3168fe925a67962554f
Achmad Mahardi <achmad@mahardi.me>
2015-06-18 13:37:14 +0700

    wat

--- js/angular/services/foundation.core.js
+++ js/angular/services/foundation.core.js
@@ -168,9 +168,9 @@
     // }
 
     // Attach viewport units buggyfill
-    if (viewportUnitsBuggyfill) {
-      viewportUnitsBuggyfill.init();
-    }
+    // if (viewportUnitsBuggyfill) {
+    //   viewportUnitsBuggyfill.init();
+    // }
   }
 
 })();
